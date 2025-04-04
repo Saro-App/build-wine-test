4175ad6ad038a30b29d08793c6c4ea570a0768a8
Joshua Chan <121840186+Melonchanism@users.noreply.github.com>
2025-04-03 22:32:02 -0700

    Add all dependencies
    IT WORKS !!!1!1!1
    Brew for all arches
    Stuff from https://gitlab.winehq.org/wine/wine/-/wikis/MacOS-Building

--- build.sh
+++ build.sh
@@ -1,29 +1,37 @@
 #!/bin/sh
 
-/bin/sh clean.sh
+softwareupdate --install-rosetta --agree-to-license
+
+#Install homebrew for both regular and rosetta
+NONINTERACTIVE=1
+if ! [ -f /opt/homebrew/bin/brew ]; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)";
+    echo >> ~/.zprofile;
+    echo eval '"$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile;
+    eval "$(/opt/homebrew/bin/brew shellenv)";
+fi;
+if ! [ -f /usr/local/bin/brew ]; then
+    arch -x86_64 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)";
+    echo >> ~/.zprofile
+    echo eval '"$(/usr/local/bin/brew shellenv)"' >> ~/.zprofile
+    eval "$(/usr/local/bin/brew shellenv)"
+fi;
+
+/opt/homebrew/bin/brew install --formula bison mingw-w64 pkgconfig wget
+arch -x86_64 /usr/local/bin/brew install --formula freetype gnutls molten-vk sdl2
 
 export CC="arch -x86_64 cc"
 export CXX="arch -x86_64 c++"
 export CPP="arch -x86_64 cpp"
 export CFLAGS="-m64"
 
-echo "Installing dependencies..."
-
-mkdir -p dependencies
-
-brew install bison
-export PATH="/opt/homebrew/opt/bison/bin:$PATH"
-
-brew install mingw-w64 gettext
-
-/bin/sh build-libinotify-kqueue.sh || { echo "Failed to install 'libinotify-kqueue'"; exit 1; }
 
 URL="https://media.codeweavers.com/pub/crossover/source/crossover-sources-25.0.0.tar.gz"
 TAR_FILE="crossover-sources-25.0.0.tar.gz"
 EXPECTED_SHA256="b0f3c1263bb1d7bfb8afa63493550be832ca55cd5f3d0bd2c9077991638d4e44"
 
 echo "Downloading $TAR_FILE..."
-wget -q "$URL" -O "$TAR_FILE" || { echo "Failed to download $TAR_FILE"; exit 1; }
+wget -q "$URL" -O "$TAR_FILE" || { echo "Failed to download $TAR_FILE"; }
 
 echo "Verifying checksum..."
 ACTUAL_SHA256=$(sha256sum "$TAR_FILE" | awk '{ print $1 }')
@@ -40,6 +48,9 @@ tar -xzf "$TAR_FILE" || { echo "Failed to extract $TAR_FILE"; exit 1; }
 cd sources || { echo "Failed to enter directory 'crossover-sources-25.0.0'"; exit 1; }
 cd wine || { echo "Failed to enter 'wine' directory"; exit 1; }
 
+brew install bison
+export PATH="/opt/homebrew/opt/bison/bin:$PATH"
+
 echo "Running configure..."
 ./configure \
     --host=x86_64-darwin \
@@ -51,15 +62,12 @@ echo "Running configure..."
     --with-coreaudio \
     --with-cups \
     --without-dbus \
-    --with-ffmpeg \
     --with-freetype \
     --with-gettext \
     --without-gettextpo \
     --without-gphoto \
     --with-gnutls \
     --without-gssapi \
-    --with-gstreamer \
-    --with-inotify \
     --without-krb5 \
     --with-mingw \
     --without-netapi \
@@ -80,6 +88,7 @@ echo "Running configure..."
     --without-wayland \
     --without-x \
     || { echo "Configure failed"; exit 1; }
+# Note ffmpeg, libinotify, gstreamer removed
 
 echo "Running make..."
 make || { echo "Make failed"; exit 1; }
