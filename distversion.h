4b0754f49e2d17785e958c41857276e384dd664b
Joshua Chan <121840186+Melonchanism@users.noreply.github.com>
2025-04-03 22:33:36 -0700

    Add distversion.h, a missing file from the code weavers wine

---
+++ distversion.h
@@ -0,0 +1,16 @@
+// Copied from https://github.com/winedbg/build-wine-test/new/build-script-working
+// Move to wine/programs/winedbg/distversion.h
+
+
+/* ---------------------------------------------------------------
+ *   distversion.c
+ *
+ * Copyright 2013, CodeWeavers, Inc.
+ * 
+ * Information from DISTVERSION which needs to find 
+ * its way into the wine tree.
+ * --------------------------------------------------------------- */
+
+#define WINDEBUG_WHAT_HAPPENED_MESSAGE "This can be caused by a problem in the program or a deficiency in Wine."
+
+#define WINDEBUG_USER_SUGGESTION_MESSAGE "Check the log for any missing native dependencies and install using winetricks"
