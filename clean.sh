b028ada98b3b4748a8f60b146c7ca27f1415bf18
Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>
2025-04-05 04:22:01 -0400

    chore: Add copyright notice and maybe fix cache
    Signed-off-by: Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>

--- clean.sh
+++ clean.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# Copyright (C) 2025 Ethan Uppal and @Melonchanism
+# 
+# This file is part of build-wine-test.
+# build-wine-test is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+# build-wine-test is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with build-wine-test. If not, see <https://www.gnu.org/licenses/>.
+
 SOURCES="sources"
 DEPENDENCIES="dependencies"
 TAR="crossover-sources-25.0.0.tar.gz"
--- silicon-driver.sh
+++ silicon-driver.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
+# Copyright (C) 2025 Ethan Uppal and @Melonchanism
+# 
+# This file is part of build-wine-test.
+# build-wine-test is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+# build-wine-test is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with build-wine-test. If not, see <https://www.gnu.org/licenses/>.
+
 /bin/sh clean.sh
 arch -x86_64 /bin/sh build.sh
